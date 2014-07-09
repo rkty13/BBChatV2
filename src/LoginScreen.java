da885e22397c213514799210dabaf6e6adce0cd6
ekongg <elk2son@yahoo.com>
2014-07-09 15:28:14 -0400

    Making the change from Swing to JavaFX

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -1,29 +1,6 @@
-import java.awt.Container;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JEditorPane;
 
 
 
-public class LoginScreen extends JFrame{
-    private static final long serialVersionUID = 1L;
-    
-    
-    public LoginScreen(){
-    	
-    	
-    	final Container mainPanel = getContentPane();
-    	
-    	pack();
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		setLocationRelativeTo(null);
-		setResizable(false);
-    	setVisible(true);
-    }
+public class LoginScreen{
+
 }
