83a0707bcf165494b77b781080cb04c3ca3f60a0
ekongg <elk2son@yahoo.com>
2014-07-09 16:14:15 -0400

    CSS and JavaFX

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -1,6 +1,53 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-
-
-public class LoginScreen{
-
+public class LoginScreen extends Application{
+	
+	Label label;
+	Button button;
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+	    
+		DropShadow ds = new DropShadow();
+		ds.setOffsetY(5.0);
+		ds.setOffsetX(5.0);
+		ds.setColor(Color.WHITE);
+		
+		label = new Label("Test");
+	    button = new Button("Click");
+	    
+	    button.setEffect(ds);
+	    
+	    label.getStyleClass().add("my_customlabel");
+	    
+	    button.setOnAction(new EventHandler<ActionEvent>(){
+	    	@Override
+	    	public void handle(ActionEvent args0){
+	    		label.setText("Test 123456789");
+	    	}
+	    });
+	    
+	    VBox root = new VBox();
+	    root.getChildren().addAll(label, button);
+	    Scene scene = new Scene(root, 500, 500);
+	    stage.setScene(scene);
+	    
+	    scene.getStylesheets().add("style.css");
+	    
+	    stage.show();
+	    
+    }
+	
+	public static void main(String[]args){
+		launch(args);
+	}
 }
