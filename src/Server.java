7bf509e3903a24e8af56d4ed06f5662e61f330da
Robert Kim <rkty13@gmail.com>
2014-07-09 00:08:13 -0400

    Edited Server class

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,20 @@
-
-import org.json.simple.JSONObject;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Server {
+
+	public Server() {
+		ServerSocket connectUser;
+		try {
+			connectUser = new ServerSocket(Integer.valueOf(System
+					.getenv("PORT")));
+		} catch (IOException e) {
+
+		}
+	}
+
+	public static void main(String[] args) {
+		new Server();
+	}
 
 }
