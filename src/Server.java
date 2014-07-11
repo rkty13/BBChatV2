3b99e9e3380e83e95db844bbfeac072f7df3b2f3
Robert Kim <rkty13@gmail.com>
2014-07-11 16:06:19 -0400

    Revert "?"
    This reverts commit 7ecf0da31a328452923696fdf4aaa3975324663e.

--- src/Server.java
+++ src/Server.java
@@ -1,24 +1,13 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
 
-	@SuppressWarnings("resource")
 	public Server() {
 		ServerSocket connectUser;
 		try {
 			connectUser = new ServerSocket(Integer.valueOf(System
 					.getenv("PORT")));
-
-			while (true) {
-				Socket connection = null;
-				try {
-					connection = connectUser.accept();
-				} catch(IOException e) {
-					
-				}
-			}
 		} catch (IOException e) {
 
 		}
