cee34c9a6b2c47c74f1875fec18ed8598e669f8c
ekongg <elk2son@yahoo.com>
2014-07-13 13:43:40 -0400

    Preparing the LoginScreen

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -6,45 +6,30 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Reflection;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class LoginScreen extends Application{
 	
-	Label label;
-	Button button;
-	
+
 	@Override
     public void start(Stage stage) throws Exception {
-	    
-		DropShadow ds = new DropShadow();
-		ds.setOffsetY(5.0);
-		ds.setOffsetX(5.0);
-		ds.setColor(Color.WHITE);
 		
-		Reflection reflection = new Reflection();
-		reflection.setFraction(0.8);
-		reflection.setTopOffset(-20);
-		
-		label = new Label("Test");
-	    button = new Button("Click");
+	
 	    
-	    button.setEffect(ds);
-	    label.setEffect(reflection);
+	    BorderPane border = new BorderPane();
 	    
-	    label.getStyleClass().add("my_customlabel");
 	    
-	    button.setOnAction(new EventHandler<ActionEvent>(){
-	    	@Override
-	    	public void handle(ActionEvent args0){
-	    		label.setText("Test 123456789");
-	    	}
-	    });
+	
+	
 	    
-	    VBox root = new VBox();
-	    root.getChildren().addAll(label, button);
-	    Scene scene = new Scene(root, 500, 500);
+	    Scene scene = new Scene(border);
+	    
+	    stage.setTitle("BBChat V2.0");
+	    //stage.getIcons().add(new Image (""));
 	    stage.setScene(scene);
 	    
 	    scene.getStylesheets().add("style.css");
